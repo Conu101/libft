bf760402a6b7b86cc4742018110a4472540b59ce
unknown <constance.trouve@gmail.com>
2021-11-25 11:47:38 +0200

    test

--- test_conu/test_ft_is_alpha/main.c
+++ test_conu/test_ft_is_alpha/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int	ft_isalpha(int c);
+int		ft_isalpha(int c);
 
 int		main(void)
 {
@@ -10,21 +10,46 @@ int		main(void)
 	i = 'A';
 	while (i <= 'Z')
 	{
-		printf("\n %d \n", isalpha(i));
-		ft_isalpha(i);
+		printf("\n A to Z %d \n", isalpha(i));
+		printf("\n %d \n", ft_isalpha(i));
 		printf("\n");
 		i++;
 	}
-
 
 	i = 'a';
 	while (i <= 'z')
 	{
-		printf("\n %d \n", isalpha(i));
-		ft_isalpha(i);
+		printf("\n a to z %d \n", isalpha(i));
+		printf("\n %d \n", ft_isalpha(i));
 		printf("\n");
 		i++;
 	}
 
+	i = 0;
+	while (i <= 64)
+	{
+		printf("\n below A %d \n", isalpha(i));
+		printf("\n %d \n", ft_isalpha(i));
+		printf("\n");
+		i++;
+	}
+
+	i = 91;
+	while (i <= 96)
+	{
+		printf("\n between z and A %d \n", isalpha(i));
+		printf("\n %d \n", ft_isalpha(i));
+		printf("\n");
+		i++;
+	}
+
+	i = 123;
+	while (i <= 127)
+	{
+		printf("\n over Z %d \n", isalpha(i));
+		printf("\n %d \n", ft_isalpha(i));
+		printf("\n");
+		i++;
+	}
 	return (0);
 }
